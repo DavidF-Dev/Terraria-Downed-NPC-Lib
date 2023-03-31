98f6967fd3dc448409404cb0ff5cbb67397d488d
David F <account@davidfdev.com>
2023-03-31 17:32:51 +1100

    Added TODO: implement mod calls

--- Code/DownedNPCLibMod.cs
+++ Code/DownedNPCLibMod.cs
@@ -10,4 +10,20 @@ namespace DownedNPCLib;
 // ReSharper disable once ClassNeverInstantiated.Global
 internal sealed class DownedNPCLibMod : Mod
 {
+    #region Static Fields and Constants
+
+    private const byte ModCallGetByNetId = 0;
+    private const byte ModCallGetByType = 1;
+
+    #endregion
+
+    #region Methods
+
+    public override object Call(params object[] args)
+    {
+        // TODO: Implement mod calls
+        return null;
+    }
+
+    #endregion
 }
